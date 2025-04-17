2f635c88df76aef2a11aa833944bcc23faef5ce8
Aishwarya Sreejith <121996414+AishRev@users.noreply.github.com>
2025-04-17 14:57:19 +0530

    Update Claven.jsx

--- frontend/src/pages/Claven.jsx
+++ frontend/src/pages/Claven.jsx
@@ -1,12 +1,1 @@
-import React from 'react'
 
-
-const Claven = () => {
-    return (
-      <div className="bg-green-500 text-red-500 p-4 rounded shadow">
-        Tailwind is finally working! 🌱
-      </div>
-    );
-  };
-
-export default Claven;  
