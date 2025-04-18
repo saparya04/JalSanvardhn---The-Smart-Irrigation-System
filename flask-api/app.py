a6bc5be0b43edefe145188d9b344999a34ecf4c3
saparya04 <saparyadey2019@gmail.com>
2025-04-18 13:09:37 +0530

    deploy

--- flask-api/app.py
+++ flask-api/app.py
@@ -436,4 +436,4 @@ def predict_disease():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5002)))
