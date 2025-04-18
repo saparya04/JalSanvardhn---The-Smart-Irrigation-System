f345c5761c256d7b2c8439dc0cdcef72791c482e
saparya04 <saparyadey2019@gmail.com>
2025-04-18 12:59:33 +0530

    route chnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login.jsx";
 import CropData from "./pages/CropData";
@@ -7,27 +7,25 @@ import WaterFlowInput from "./pages/WaterFlowInput";
 import PicData from "./pages/PicData";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css'
+import './App.css';
 import Home from './pages/Home.jsx';
-// import '../src/global.css';
-// import Layout from "./components/ui/layout.jsx";
-
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/crop-data" element={<CropData />} />
         <Route path="/water-flow" element={<WaterFlowInput />} />
         <Route path="/home" element={<Home />} />
         <Route path="/pic-data" element={<PicData />} />
-        
       </Routes>
     </Router>
   );
 }
+
 export default App;
 //new change
 
